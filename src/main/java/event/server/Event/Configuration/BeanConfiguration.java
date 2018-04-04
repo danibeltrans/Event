8c62ccb8bf18d13747f0527dcd067333736f18d9
Sebastian Higuita <sebastianhiguita@MacBook-Air-de-Sebastian.local>
2018-04-03 22:41:46 -0500

    Se pasa de.properties a .yml / creación de contexto para el sessionFactory

---
+++ src/main/java/event/server/Event/Configuration/BeanConfiguration.java
@@ -0,0 +1,17 @@
+package event.server.Event.Configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+
+@EnableAutoConfiguration
+public class BeanConfiguration {
+    @Bean
+    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+        HibernateJpaSessionFactoryBean fact = new HibernateJpaSessionFactoryBean();
+        fact.setEntityManagerFactory(emf);
+        return fact;
+    }
+}
